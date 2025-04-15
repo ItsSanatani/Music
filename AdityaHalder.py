852477ffa190e9ec9c71e8b19254df1ffead937a
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-16 01:52:28 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -213,8 +213,8 @@ async def main():
         LOGGER.info(f"🚫 Assistant Error: {e}")
         sys.exit()
     try:
-        await app.join_chat("AdityaServer")
-        await app.join_chat("AdityaDiscus")
+        await app.join_chat("SANATANI_TECH")
+        await app.join_chat("SANATANI_SUPPORT")
     except Exception:
         pass
     if LOG_GROUP_ID != 0:
@@ -970,7 +970,7 @@ async def stream_logger(
         # Optional: check if thumbnail is valid (if it's a URL or file path)
         if thumbnail:
             try:
-                await bot.send_photo(LOG_GROUP_ID, photo=thumbnail, caption=caption)
+                await app.send_photo(LOG_GROUP_ID, photo=thumbnail, caption=caption)
                 print(f"Log sent to {LOG_GROUP_ID}")
             except Exception as e:
                 print(f"Error sending photo to log group: {e}")
@@ -1018,7 +1018,7 @@ async def stream_logger(
 **🌼 Chat Link:** {chat_link}
 **👾 Requested By:** {requested_by}"""
             try:
-                await bot.send_photo(LOG_GROUP_ID, photo=thumbnail, caption=caption)
+                await app.send_photo(LOG_GROUP_ID, photo=thumbnail, caption=caption)
             except Exception:
                 pass
 
