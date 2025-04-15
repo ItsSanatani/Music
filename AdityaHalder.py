2bf0e081733b07c8cec77e73bd11f7b49ca775c6
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-16 01:55:19 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -980,48 +980,6 @@ async def stream_logger(
         print(f"Logger encountered an error: {e}")
 
 #=×=×=×=×=×=×=×=××=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
-
-async def stream_logger(
-    chat_id, user, title, duration, stream_type, thumbnail, position=None
-):
-    if LOG_GROUP_ID != 0:
-        if chat_id != LOG_GROUP_ID:
-            chat = await bot.get_chat(chat_id)
-            chat_name = chat.title
-            if chat.username:
-                chat_link = f"@{chat.username}"
-            else:
-                chat_link = "Private Chat"
-            try:
-                if user.username:
-                    requested_by = f"@{user.username}"
-                else:
-                    requested_by = user.mention
-            except Exception:
-                requested_by = user.title
-            if position:
-                caption = f"""**✅ Added To Queue At :** `#{position}`
-
-**🥀 Title:** {title}
-**🐬 Duration:** {duration}
-**🦋 Stream Type:** {stream_type}
-**🌺 Chat Name:** {chat_name}
-**🌼 Chat Link:** {chat_link}
-**👾 Requested By:** {requested_by}"""
-            else:
-                caption = f"""**✅ Started Streaming On VC.**
-
-**🥀 Title:** {title}
-**🐬 Duration:** {duration}
-**🦋 Stream Type:** {stream_type}
-**🌺 Chat Name:** {chat_name}
-**🌼 Chat Link:** {chat_link}
-**👾 Requested By:** {requested_by}"""
-            try:
-                await app.send_photo(LOG_GROUP_ID, photo=thumbnail, caption=caption)
-            except Exception:
-                pass
-
 #=×=×=×=×=×=×=×=××=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
 # Change stream & Close Stream
 
