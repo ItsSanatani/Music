3a3c47a6ec06d6c6854b757c2f7769f87d6a0fca
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-16 10:00:42 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -26,9 +26,7 @@ from git.exc import GitCommandError, InvalidGitRepositoryError
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_async_
 
 from pyrogram import Client, filters as pyrofl
-from pytgcalls.types import Update
-from pytgcalls.types.stream import StreamAudioEnded
-from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls import PyTgCalls, filters as pytgfl
 
 from pyrogram import idle, __version__ as pyro_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
